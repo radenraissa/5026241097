071c55187df5f9f6e9144d418dca54cdd1cdffb9
radenraissa <raden.muhammadraissa@gmail.com>
2026-05-25 12:10:26 +0700

    pertemuan 12

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,41 +1,43 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Tutorial Membuat CRUD Pada Laravel - www.malasngoding.com</title>
-</head>
-<body>
+@extends('template')
+@section('title', 'Data Pegawai')
+@section('konten')
+    <a href="/pegawai/tambah" class="btn btn-primary">Tambah Pegawai Baru</a>
 
-	<h2>www.malasngoding.com</h2>
-	<h3>Data Pegawai</h3>
+    <br />
+    <br />
 
-	<a href="/pegawai/tambah"> + Tambah Pegawai Baru</a>
-
+	
+	<form action="/pegawai/cari" method="GET">
+		<div class="form-group">
+			<label>Cari Data Pegawai :</label>
+			<input type="text" name="cari" placeholder="Cari Pegawai .." class="form-control">
+			<input type="submit" value="CARI" class="btn btn-light">
+		</div>
+	</form>
+		
 	<br/>
-	<br/>
 
-	<table border="1">
-		<tr>
-			<th>Nama</th>
-			<th>Jabatan</th>
-			<th>Umur</th>
-			<th>Alamat</th>
-			<th>Opsi</th>
-		</tr>
-		@foreach($pegawai as $p)
-		<tr>
-			<td>{{ $p->pegawai_nama }}</td>
-			<td>{{ $p->pegawai_jabatan }}</td>
-			<td>{{ $p->pegawai_umur }}</td>
-			<td>{{ $p->pegawai_alamat }}</td>
-			<td>
-				<a href="/pegawai/edit/{{ $p->pegawai_id }}">Edit</a>
-				|
-				<a href="/pegawai/hapus/{{ $p->pegawai_id }}">Hapus</a>
-			</td>
-		</tr>
-		@endforeach
-	</table>
-
-
-</body>
-</html>
+    <table border="table" class="table table-bordered table-striped table-hover">
+        <tr>
+            <th>Nama</th>
+            <th>Jabatan</th>
+            <th>Umur</th>
+            <th>Alamat</th>
+            <th>Opsi</th>
+        </tr>
+        @foreach ($pegawai as $p)
+            <tr>
+                <td>{{ $p->pegawai_nama }}</td>
+                <td>{{ $p->pegawai_jabatan }}</td>
+                <td>{{ $p->pegawai_umur }}</td>
+                <td>{{ $p->pegawai_alamat }}</td>
+                <td>
+                    <a href="/pegawai/edit/{{ $p->pegawai_id }}" class="btn btn-sm btn-warning">Edit</a>
+                     
+                    <a href="/pegawai/hapus/{{ $p->pegawai_id }}" class="btn btn-sm btn-danger">Hapus</a>
+                </td>
+            </tr>
+        @endforeach
+    </table>
+	{{ $pegawai->links() }}
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,9 @@ Route::post('formulir/proses', [PegawaiController::class, 'proses']);
 
 //crud table pegawai
 Route::get('/pegawai', [PegawaiDBController::class, 'index']);
+Route::get('/pegawai/tambah', [PegawaiDBController::class, 'tambah']);
+Route::post('/pegawai/store', [PegawaiDBController::class, 'store']);
+Route::get('/pegawai/edit/{id}', [PegawaiDBController::class, 'edit']);
+Route::post('/pegawai/update', [PegawaiDBController::class, 'update']);
+Route::get('/pegawai/hapus/{id}', [PegawaiDBController::class, 'hapus']);
+Route::get('/pegawai/cari', [PegawaiDBController::class, 'cari']);
